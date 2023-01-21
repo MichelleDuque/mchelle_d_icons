76b5a5d3f245831f66b454d6b245b05942bd5764
MichelleDuque <michelled_1994@hotmail.com>
2023-01-21 03:42:24 -0500

    changes added in html css and js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,21 @@
-console.log("Javascript is running");
+console.log('JavaScript is running!');
 
-/* Longer JS Block comment
+/* longer JS block comment 
 - const is a type of variable - a bit of memory that can hold a value
 
-JS used (can use) any valid CSS selector to make a connection to any element on the page - it works the same as CSS selectors do,
-because it's using css selectror to find matvhing element(s).
+JS uses (can use) ANY valid CSS selector to make a connection to ANY elment on the page - it works the same way as CSS selectors do, because it's USING css selectors to find matching element(s).
 
-The document is the DOM - the web page and all of its elements
+the document is the DOM - the web page and all of its elements
 
-queryselector is the method (function) that makes the connection between JS and the Dom (the elements)
-
+querySelector is the method (function) that makes the connection between JS and the DOM (the elements)
 */
 
-// step 1 - Make the connection to the element(s) you want to interact with
+// step 1 - make the connection to the element(s) you want to interact with
+const vectorGraphic = document.querySelector('#newBadge');
 
-const jpgGraphic = document.querySelector("#bitmap"); 
-const svgGraphic = document.querySelector("#vector"); 
-
-// step 3 - create function
-function logID(){
-    console.log("click on this element:", this.id);
+function logThisId() {
+    console.log('clicked on this element:', this.id);
 }
 
-
-
-// step 2 - decide how you want the user to ineract with object you created in step 1
-svgGraphic.addEventListener("click", logID);
-jpgGraphic.addEventListener("click", logID);
+// step 2 - decide how you want the user to interact with the obect you created in step 1
+vectorGraphic.addEventListener('click', logThisId);
